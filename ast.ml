b4a51dbd98ae1f0a25b5751596e56bcb1c0bfa36
Gael-Bernard <gael.berbere@gmail.com>
2022-01-09 20:45:16 +0100

    Grand nettoyage des types ast.ml

--- ast.ml
+++ ast.ml
@@ -1,128 +1,130 @@
-(* Pour les type string et integer *)
-type var_type = 
-  | String of string 
-  | Integer of int 
+(**
+  ___________________________________________________________
+/                                ------------°°°°------------                                  \
+|                                TYPES DEFINITIONS
+|        Stockent les définitions déclarés dans le programme
+\ __________________________________________________________ /
+**)
+
+type prog_def = {
+  classes : class_def list;
+  program : block_t 
+} 
   
 
-type class_type = {
+type class_def = {
   name : string; 
-  superClasse : string option; 
-  attributs : decl list; (* param + attribut *)
-  meth : methode list;
-  construct : constructor ;
-  }
+  superclass : string option; 
+  attributes : variable_def list; (* param + attribut *)
+  methods : methode_def list;
+  constructor : constructor_def ;
+}
 
-and constructor = {
-  name_constuctor : string;
-  param_constuctor : decl list; 
-  body_constuctor : bloc;
-  super_call : constructor_call option
-  } 
+and constructor_def = {
+  name : string;
+  parameters : variable_def list; 
+  body : block_t;
+  super_call : superconstructor_call option
+}
 
-and decl = {
-    var : bool;
-    stati : bool;
-    typ : string;
-    nom : string;
-  }
+and superconstructor_call = {
+  superclass : string;
+  arguments : expression_t list
+}
 
-and methode = {
-  name_methode : string;
-  param_methode : decl list;
-  body_methode : bloc;
-  static_methode : bool;
-  override : bool;
-  retour_methode : string option  (* le type est un string ex : int est INTEGER *)
+and methode_def = {
+  name : string;
+  parameters : variable_def list;
+  body : block_t;
+  is_static : bool;
+  is_override : bool;
+  return_type : string option  (* le type est un string ex : int est INTEGER *)
+}
+
+and variable_def = {
+  name : string;
+  is_var : bool;
+  is_static : bool;
+  typ : string;
 }
 
 
 
-and instrType = 
-    Exp of exp_type
-  | Bloc of bloc
-  | Ite of exp_type*instrType*instrType
+(**
+  ___________________________________________________________
+/                                ------------°°°°------------                                  \
+|                               TYPES INSTRUCTIONS
+|        Stockent les différentes instructions dans les blocs
+\ __________________________________________________________ /
+**)
+
+
+and block_t = {
+  declarations : variable_def list;
+  instructions : instruction_t list;
+}
+
+and instruction_t = 
+    Exp of expression_t
+  | Block of block_t
+  | Ite of expression_t * instruction_t * instruction_t
   | Return 
-  | Affectation of exp_type * exp_type
+  | Affectation of container * expression_t
   
 
-and bloc = {
-  declarations : decl list;
-  instructions : instrType list;
-}
+and container_t =
+  | Select of selection_beg_t
+  | LocalVar of string
 
 
-and constructor_call = {
-  classe : string;
-  arguments : exp_type list
-}
 
- (*
-type exp_type =
- (* var_type *)
-| Cste of int (* var_type *)
-| Plus of exp_type*exp_type
-| Minus of exp_type*exp_type
-| Times of exp_type*exp_type
-| Div of exp_type*exp_type
-| UMinus of exp_type
-| Comp of opComp*exp_type*exp_type
-| Selection of exp_type*Id (* Selection expression *)
-| Instantiation of Id * listArgOpt (* Instanciation expression *)
-| Cast of classType * exp_type (* (nomClasse expression) *)
-| Envoi of (* envoie de message*)
-*) 
 
-and exp_type =
+
+(**
+  ___________________________________________________________
+/                                ------------°°°°------------                                  \
+|                                TYPES EXPRESSIONS
+|                   Stockent des expressions à évaluer
+\ __________________________________________________________ /
+**)
+
+and expression_t =
+  | IntLiteral of int
+  | StringLiteral of string
   | Id of string
   | Result
-  | Selection of exp_type*exp_type
-  (*| Assignment of assignment
-  | Conditional of expr_e * (expr_e * expr_e) *)
-  | Binary of binary_op * exp_type * exp_type
-  | Unary of unary_op * exp_type
-  (*| Variable of *)
-  (*| Integer_constant of Int64.t
-  | Float_constant of float
-  | Bool_constant of bool
-  | Null
-  | Char_constant of string
-  | String_constant of string *)
+  | SelectionChain of selection_t
+  | Binary of binary_operator_t * expression_t * expression_t
+  | Unary of unary_operator_t * expression_t
+  | Cast of string * expression_t (* string = Nom de la classe de destination ; expression_t = Valeur à caster *)
   | This
-  | Cste of int 
-  (*| New of instance_creation
-  | Field_access of  field_obj * string *)
-  | Method_call of methode * (exp_type list) (* S'assurer que la méthode renvoie quelque chose à l'analyse contextuelle *)
-  | Comp of opComp*exp_type*exp_type
+  | Method_call of methode_def * (expression_t list) (* S'assurer que la méthode renvoie quelque chose à l'analyse contextuelle *)
+  | Comp of compare_operator_t*expression_t*expression_t
+  | NewClass of string * expression_t list
 
-  and unary_op =
-    | Not
-    | Cast of var_type
-    | Complement
-    | Pre_decr
-    | Post_decr
-    | Pre_incr
-    | Post_incr
-    | Minus
+  and unary_operator_t =
+    | UMINUS
 
-and binary_op =
-  (* Logic *)
-  | Or | And | Nor
+and binary_operator_t =
+  | IntOperator of int_binary_operator_t
+  | StringConcat
+
+and int_binary_operator_t =
   (* Comparaison *)
-  | OpComp 
-  (* Arithmetic *)
-  | PLUS | MINUS | TIMES | DIV 
+  | EQ | NEQ | LT | LE | GT | GE
+  (* Arithmétique *)
+  | PLUS | MINUS | TIMES | DIV
 
-  and opComp = 
-  | Eq | Neq | Lt | Le | Gt | Ge
 
-(*
-and type_decl =
-  | Class_declaration of class_decl *)
+and selection_beg_t =
+  | ExprSelect of expression_t * selection_end_t
+  | ClassSelect of string * selection_end_t
+  | ThisSelect * selection_end_t
 
-type prog_type = {
-  cl : class_type list;
-  b : bloc 
-} 
+and selection_end_t =
+  | AttrSelect of string * selection_end_t * selection_end_t option
+  | MethSelect of string * expression_t list * selection_end_t option
+
 
 
 
@@ -144,7 +146,7 @@ type prog_type = {
 \ ___________________________________________ /
 **)
 
-type attrsMethsConstructor = {attrs: decl list; meths: methode list; construct: constructor option}
+type attrsMethsConstructor = {attrs: variable_def list; meths: methode_def list; construct: constructor_def option}
 
 
 
@@ -158,16 +160,16 @@ type attrsMethsConstructor = {attrs: decl list; meths: methode list; construct: 
 
 
 let getAttrsFromAMCList (amcList:attrsMethsConstructor list) =
-  List.fold_left (fun (acc:decl list) (attrib:decl) -> attrib::acc ) [] (* Inverse la liste à nouveau *)
-    (List.fold_left (fun (acc:decl list) (amc:attrsMethsConstructor) ->
-      (List.fold_left (fun (acc:decl list) (attrib:decl) -> attrib::acc) acc amc.attrs))
+  List.fold_left (fun (acc:variable_def list) (attrib:variable_def) -> attrib::acc ) [] (* Inverse la liste à nouveau *)
+    (List.fold_left (fun (acc:variable_def list) (amc:attrsMethsConstructor) ->
+      (List.fold_left (fun (acc:variable_def list) (attrib:variable_def) -> attrib::acc) acc amc.attrs))
     [] amcList)
 ;;
 
 let getMethsFromAMCList (amcList:attrsMethsConstructor list) =
-  List.fold_left (fun (acc:methode list) (meth:methode) -> meth::acc ) [] (* Inverse la liste à nouveau *)
-    (List.fold_left (fun (acc:methode list) (amc:attrsMethsConstructor) ->
-      (List.fold_left (fun (acc:methode list) (meth:methode) -> meth::acc) acc amc.meths)
+  List.fold_left (fun (acc:methode_def list) (meth:methode_def) -> meth::acc ) [] (* Inverse la liste à nouveau *)
+    (List.fold_left (fun (acc:methode_def list) (amc:attrsMethsConstructor) ->
+      (List.fold_left (fun (acc:methode_def list) (meth:methode_def) -> meth::acc) acc amc.meths)
     ) [] amcList)
 ;;
 
