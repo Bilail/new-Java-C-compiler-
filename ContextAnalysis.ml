c5e820cf6703368174ea5a133e3950a81a35980d
Gael Bernard <gbernar@a-999903.ups.u-psud.fr>
2022-01-17 16:25:33 +0100

    Progression TP : 3 nouvelles vérifs

--- ContextAnalysis.ml
+++ ContextAnalysis.ml
@@ -6,7 +6,7 @@ open Ast
 (**
   ____________________________________________
 /                    ------------°°°°------------                      \
-|     				ACTUELLEMENT TESTE
+|     				ACTUELLEMENT TESTE                     
 \ ___________________________________________ /
 **)
 (**
@@ -15,17 +15,18 @@ open Ast
     1. Plusieurs classes ne peuvent pas avoir le même nom
     2. Les paramètres de la classe sont les mêmes que les params du constructeur
     3. Le constructeur et sa classe extend-ent la même superclasse
-    L'appel à la superclasse dans le constructeur a des arguments qui correspondent à la superclasse
+    4. On doit extend une classe qui existe (pas forcément déclarée plus haut)
+    5. On ne peut pas s'extend soi-même // A tester
+    6. On ne peut pas faire d'héritage circulaire // A tester
 
 
 
 A faire
     Les params du constructeur correspondent aux args de l'instanciation
     Les params des méthodes correspondent aux arguments des appels
-    On doit extend une classe qui existe (pas forcément déclarée plus haut)
-    On ne peut pas faire d'héritage circulaire
+    L'appel à la superclasse dans le constructeur a des arguments qui correspondent à la superclasse
     On ne peut pas déclarer de variables locales avec le même nom dans la même portée
-    
+    Les membres gauche et droit d'une affectation doivent avoir même type
 
 **)
 
@@ -33,18 +34,32 @@ A faire
 (**
   ____________________________________________
 /                    ------------°°°°------------                      \
-|     		FONCTIONS UTILES PLUS BAS
+|     		FONCTIONS UTILES PLUS BAS               
 \ ___________________________________________ /
 **)
 
+
+
+
+(* Compte les occurrences d'un élément dans une liste *)
+let countOccurrences elem lis =
+    List.fold_left (fun count el -> if elem = el then count+1 else count) 0 lis
+
+
+(* Indique si un nom de classe existe dans une liste *)
+let countClassnameAmong classes name =
+    (countOccurrences name (List.map (fun c -> c.name_class) classes))
+
+
 (* Affiche une erreur si "name" est un nom porté par une classe dans "classes" *)
 let forbidClassName (classes:class_def list) name =
-	let check = not (List.exists (fun c -> c.name_class = name) classes)
+	let check = ((countClassnameAmong classes name) = 1)
 	in match check with
 	| true -> true
 	| false ->
 		print_string "[Error] Two classes cannot have the same name : "; print_string name; print_newline ();
 		false
+
 
 (* Affiche une erreur si les paramètres d'une classe et de son constructeur sont différents *)
 let chckParamsInClaAndConstr c =
@@ -54,16 +69,45 @@ let chckParamsInClaAndConstr c =
     | false -> 
         print_string "[Error] A class and its constructor must have the same parameters in "; print_string c.name_class;
         false
-  
+ 
+(* Affiche une erreur si la superclasse n'existe pas *)
+let chckSuperclassExistence call c classes =
+    let check = (countClassnameAmong classes call.superclass_constructor) = 1
+    in match check with
+    | true -> true
+    | false ->
+        (print_string "[Error] "; print_string c.name_class; print_string " class extends a class that doesn't exist : "; print_string call.superclass_constructor; print_newline (); (print_string "Superclasse existe OK : "; print_string call.superclass_constructor; print_newline ());
+        false)
+
+
+(* Affiche une erreur si une classe hérite d'elle-même *)
+let forbidInheritanceCycle c classes =
+    let check =
+        match c.superclass with
+        | Some supername -> not (is_subclass supername c.name_class classes) 
+        | None -> true
+    in match check with
+    | true -> true
+    | false ->
+        (print_string "[Error] Class "; print_string c.name_class; print_string " inherits itself, which is forbidden"; print_newline ());
+        false
+
+
+
+(* Affiche une erreur s'il y a un problème dans le graphe d'héritage *)
+let chckSuperclass call c (classes:class_def list) =
+    chckSuperclassExistence call c classes &&
+    true
+
 
 (* Affiche une erreur si la classe et son constructeur n'appellent pas la même superclasse *)
-let chckSuperclassInClaAndConstr c = 
+let chckSuperclassInClaAndConstr c classes = 
     match c.superclass with
     | Some s ->
       (match c.constructor.super_call with
       | Some call ->
         (if call.superclass_constructor = s then
-          true
+          chckSuperclass call c classes
         else
           (print_string "[Error] Mismatch between the superclass of "; print_string c.name_class; print_string " and the superclass called by its constructor : "; print_string s; print_string " and "; print_string call.superclass_constructor; print_newline ();
           false)
@@ -80,6 +124,18 @@ let chckSuperclassInClaAndConstr c =
       | None ->
         true
       )
+
+
+    
+
+
+(*
+let chckSuperclassCallParams call env =
+TODO : A faire quand les expressions renverront leur type 
+*) 
+    
+
+
 
 
 
@@ -102,7 +158,7 @@ let emptyEnv = {decl_classes=[]; decl_vars=[]; is_correct_env=true}
 
 
 
-let analyseClass c env =
+let analyseClass env c =
 	let newEnv = {
 		decl_classes = c::env.decl_classes;
 		decl_vars = env.decl_vars;
@@ -110,16 +166,22 @@ let analyseClass c env =
 			env.is_correct_env &&
 			forbidClassName env.decl_classes c.name_class &&
       chckParamsInClaAndConstr c &&
-      chckSuperclassInClaAndConstr c
+      chckSuperclassInClaAndConstr c env.decl_classes
 	}
     in
     (match newEnv.is_correct_env with
     | true -> print_string c.name_class; print_string " OK"; print_newline ()
     | false -> print_string c.name_class; print_string " KO"; print_newline ());
-    newEnv
+    newEnv.is_correct_env
 
 
 let analyseProgram prog =
-	List.fold_left (fun env c -> analyseClass c env) emptyEnv prog.classes
+    let env = {
+        decl_classes = prog.classes;
+        decl_vars = [];
+        is_correct_env = true
+    }
+    in
+	List.fold_left (fun is_correct c -> (analyseClass env c) && is_correct) true prog.classes
 
 
