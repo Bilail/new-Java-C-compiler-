358fc7523925e8beb5503103a2c830a5a743e013
Bilail <bilail.benyahia@outlook.fr>
2022-01-17 16:41:15 +0100

    Progrès (par Gaël)
    ----- BEN YAHIA Bilail

--- Instr_Expre_Context.ml
+++ Instr_Expre_Context.ml
@@ -10,12 +10,12 @@ let emptyEnv = {decl_classes=[]; decl_vars=[]; is_correct_env=true}
 let expr_verif expr env =
   match expr with 
   | IntLiteral i -> (*add_env_var env i*)
-  | StringLiteral s -> (*add_env_var env s*)
+  | StringLiteral s -> in_env env s 
   | Container c  -> container_verif c env
   | Method m -> methode_verif m env 
   | Binary (op,e1,e2) -> binary_verif op e1 e2 env 
   | Unary (u,e) -> unary_verif u e env
-  | Cast(s,e) -> 
+  | Cast(s,e) -> is_subclass s e env.decl_classes
   | NewClasse (s,e_list) -> add_env_classe env c (* A vérifier *)
 
 let container_verif c env = 
@@ -55,18 +55,41 @@ let instr_verif instr env =
   match instr with 
   | Exp(e) -> expr_verif e env
   | Block(b) -> block_verif b env
-  | Ite(si, alors, sinon) -> 
+  | Ite(si, alors, sinon) ->  expr_verif si env; 
+                              instr_verif alors env; 
+                              instr_verif alors env
   | Return ->
   | Affectation(c,e) -> (* Vérification des types module heritage *)
+    (* On doit vérifié que le type de e est un type ou sous type de c *)
 
 
 let block_verif b env =
+  
 
 
 
 (*-----------------------------------------------------------------------------------------------
                               Fonction Utile pour les verifs 
 -----------------------------------------------------------------------------------------------*)
+
+(* Verifie qu'une variable est dans l'envrionnement *)
+let in_env env e = 
+  List.find(fun c -> c.name == e) env.decl_vars
+
+(*Vérifier qu'un type est sous type d'un autre type *)
+let rec is_subclass m f l_cl = 
+  let mere = find_classe mere l_cl in 
+  let fille = find_classe f l_cl in 
+  if (mere.name_classe == fille.name_classe) then true 
+  else 
+    match fille.superclass with 
+      | none -> false 
+      | Some s -> is_subclass m c
+      
+(* Retoune la classe associé au nom de classe donnée en param*)
+let find_class s l_c =
+  List.find(fun c -> c.name_class == s) lc
+
 
 (* Ajout d'une variable à l'environnement *)
 let add_env_var env v = 
