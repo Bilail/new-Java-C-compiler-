e356b09b130965878f65fbf09a6439dcc550be80
Wassim <wrahoual@gmail.com>
2022-01-17 14:38:39 +0100

    Print fixe bug

--- print.ml
+++ print.ml
@@ -10,18 +10,20 @@ let rec printProg prog =
 
   (* Print de class_def *)
 and  printClass c =
-  print_string "class "; print_string c.name_class; print_string "( "; List.iter printVariable c.attributes;
-  print_string " )";
-  print_newline();
+  print_string "class "; print_string c.name_class; print_string "("; List.iter printVariable c.params_class;
+  print_string ")";
   (match c.superclass with 
    | None -> print_string " "
-   | Some s -> print_string "EXTENDS"; print_string s);
-  print_string "IS";
+   | Some s -> print_string "extends"; print_string s);
+  print_string "is";
   print_string " { ";
+  print_newline();
+  List.iter printVariable c.attributes;
+  print_newline();
+  printConstructor c.constructor;
+  print_newline();
   List.iter printMethod c.methods;
-  print_string " } ";
-  print_string " Le constructeur ";
-  (printConstructor c.constructor);
+  print_string "} ";
   (**
      ____________________________________________
      /                    ------------°°°°------------                      \
@@ -37,14 +39,15 @@ and printBloc b =
 
   (* Print de constructor_def*)
 and  printConstructor cons = 
-  print_string "DEF";
+  print_string "def ";
   print_string cons.name_constructor;
-  print_string " ( ";
+  print_string "(";
   List.iter printVariable cons.param_constructor;
   (match cons.super_call with
    | None -> print_string " "
    | Some s -> print_string " [ ";  printSuperConstructor s; print_string " ] ");
-  print_string "IS";
+  print_string ")"; 
+  print_string "is";
   printBloc cons.body_constructor;
 
   (* Print de superconstructor_call *)
@@ -54,35 +57,38 @@ and  printSuperConstructor cons =
 
   (* Print de methode_def *)
 and  printMethod m =
-  print_string " DEF ";
+  print_string " def ";
   (match m.is_static_method with 
    | false -> print_string " " 
-   | true -> print_string " STATIC");
+   | true -> print_string " static");
   (match m.is_override with 
    | false -> print_string " " 
-   | true -> print_string " OVERRIDE ");
+   | true -> print_string " override ");
   print_string m.name_method;
   print_string "("; List.iter printVariable m.param_method; print_string ")";
   (match m.return_type with 
    | None -> print_string " "
-   | Some s -> print_string " [ "; 
+   | Some s -> 
      print_string " : "; 
      print_string s; 
-     print_string " ] ";
-     print_string " IS ";
+     print_string " := ";
      printBloc m.body_method;
      print_newline());
 
   (* Print de variable_def *)
 and  printVariable d =
+  let rec printVarRec d =
   (match d.is_var with 
    | false -> print_string " "
    | true -> print_string "var ");
   (match d.is_static with 
-    | false  ->  print_string" "
-    | true -> print_string "STATIC"); 
-  print_string d.name; print_string " : "; print_string d.typ;
-  print_newline ();
+    | false  ->  print_string " "
+    | true -> print_string "static"); 
+  print_string d.name; 
+  print_string " : "; 
+  print_string d.typ;
+  print_newline();
+  in printVarRec d;
 
 
   (* Print d'instruction_t*) 
@@ -91,26 +97,24 @@ and printInstr ins =
     match ins with
     | Exp e -> printExpr e
     | Block b -> printBloc b 
-    | Return -> print_string "return ;" (* On verra jsp *)
+    | Return -> print_string " " (* On verra jsp *)
     | Ite (si, alors, sinon) ->
       print_string " IF "; printExpr si;
       print_string " THEN "; printInstrec alors;
       print_string " ELSE "; printInstrec sinon;
       print_endline "]"
     | Affectation (g, d) ->
-      print_string "["; print_string "["; printContainer g; print_string "]"; print_string ".";
-      print_string "["; printExpr d; print_string "]"; print_string "]"; print_string ";";
-      print_newline()
+      print_string " ["; printContainer g; print_string " := "; print_string "] ";  printExpr d;
   in printInstrec ins;
 
   (* Print container_t*) 
 and  printContainer c = 
   (match c with 
   | Select a -> printAttributeCall a;
-  | LocalVar n -> print_string n
-  | This -> print_string "this"
+  | LocalVar n -> print_string n;  
+  | This -> print_string "this"; print_string ".";
   | Super -> print_string "super");
-   print_newline();
+ 
 
     (* Print attribute_call*) 
 and  printAttributeCall a = 
@@ -119,7 +123,7 @@ and  printAttributeCall a =
 
   (* Print method_call*) 
 and  printMethodCall a = 
-  printCallBeginning a.beginning_call;
+  printCallBeginning a.beginning_call; 
   List.iter printCallEnd a.selections_to_meths;
 
   (* selection_beg_t *)
@@ -183,10 +187,10 @@ and  printIntBinary op  =
    List.iter printClass programme.classes;
    print_newline ();
 *)
-
+(*
 let printEnv e =
   print_string "ENVIRONMENT {"; print_newline ();
   print_string "  Classes : "; print_int (List.length e.decl_classes); print_newline ();
   print_string "  Local variables : "; print_int (List.length e.decl_vars); print_newline ();
   print_string "  Valid ? "; print_string (match e.is_correct_env with | true -> "Yes" | false -> "No"); print_newline ();
-  print_string "}"; print_newline ();
+  print_string "}"; print_newline ();*)
